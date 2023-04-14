df4a6b788d9a2f2bf31aae08085067c15c254ec6
Damian Grembocki <116957308+DamianGre@users.noreply.github.com>
2023-04-14 15:42:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,35 +8,34 @@ skorzystać ze słownika/słowników.
 */
 
 function sprawdzaczDomknięciaNapisów(napis) {
- let stos = []
- for(let znak of napis)
- if(znak == "„")
- stos.push("cudzysłów")
- else if(znak == "»")
- stos.push("szeryf")
- else if(znak == "(")
- stos.push("okrągły")
- 
- else if(znak == "”") {
- if(stos.pop() != "cudzysłów")
- return false
- }
- else if(znak == "«") {
- if(stos.pop() != "szeryf")
- return false
- }
- else if(znak == ")") {
- if(stos.pop() != "okrągły")
- return false
- }
+  let stos = []
+  for(let znak of napis)
+    if(znak == "„")
+       stos.push("cudzysłów")
+    else if(znak == "»")
+       stos.push("szeryf")
+    else if(znak == "(")
+       stos.push("okrągły")
+    else if(znak == "”") {
+       if(stos.pop() != "cudzysłów")
+         return false
+         }
+       else if(znak == "«") {
+          if(stos.pop() != "szeryf")
+          return false
+          }
+         else if(znak == ")") {
+            if(stos.pop() != "okrągły")
+            return false
+         }
  if(stos.length == 0)
-console.log("Wszystkie znaki cudzysłowy/nawiasy szeryfowe/nawiasy okrągłe zosatły zamknięte")
+     console.log("Wszystkie znaki cudzysłowy/nawiasy szeryfowe/nawiasy okrągłe zosatły zamknięte")
  else
- for(let znaki of stos)
- if(znaki == "cudzysłów")
- console.log("Cudzysłów nie został domknięty.")
- else if(znaki == "szeryf")
- console.log("Nawias szeryfowy nie został domknięty.")
- else if(znaki == "okrągły")
- console.log("Nawias okrągły nie został domknięty.")
+    for(let znaki of stos)
+        if(znaki == "cudzysłów")
+          console.log("Cudzysłów nie został domknięty.")
+        else if(znaki == "szeryf")
+          console.log("Nawias szeryfowy nie został domknięty.")
+       else if(znaki == "okrągły")
+         console.log("Nawias okrągły nie został domknięty.")
 }
